e1d0f305eb17dea80c495e51dbd20270b14149cb
iader Saavedra <109924397+RedayArdevaas@users.noreply.github.com>
2024-09-25 22:11:04 -0300

    Protección de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,37 @@
-import Navbar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
-import Home from "./views/Home";
-import Products from "./views/Products";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Register from "./views/Register";
-import Login from "./views/Login";
-import { useState } from "react";
-import CartPage from "./pages/CartPage";
-import Checkout from "./components/Checkout";
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './views/Home';
+import Login from './views/Login';
+import Register from './views/Register';
+import Products from './views/Products';
+import CartPage from './components/CartPage';
+import Checkout from './components/Checkout';
+import OrderConfirmation from './views/OrderConfirmation';
 
-function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    !!localStorage.getItem("token")
-  );
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
 
   return (
     <>
-      <Navbar
-        isAuthenticated={isAuthenticated}
-        setIsAuthenticated={setIsAuthenticated}
-      />
+      <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
       <Routes>
-        <Route path="/" element={<Home isAuthenticated={isAuthenticated}/>} />
+        <Route path="/" element={<Home isAuthenticated={isAuthenticated} />} />
+        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/registrarse" element={<Register />} />
-        <Route
-          path="/login"
-          element={<Login setIsAuthenticated={setIsAuthenticated} />}
-        />
         <Route path="/productos" element={<Products />} />
         <Route path="/carrito" element={<CartPage />} />
-        <Route path="/checkout" element={<Checkout/>}/>
-      </Routes> 
+        <Route 
+          path="/checkout" 
+          element={isAuthenticated ? <Checkout /> : <Navigate to="/login" />} 
+        />
+        <Route 
+          path="/order-confirmation" 
+          element={isAuthenticated ? <OrderConfirmation /> : <Navigate to="/login" />} 
+        />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
